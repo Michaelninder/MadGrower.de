91bad9bf315fa24fff1f768ec12459f52d9aa1a5
Michaelninder <fabianeternis@gmail.com>
2025-08-07 22:30:54 +0200

    feat: Add Theme-Switching

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,18 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const themeSwitcher = document.getElementById('theme-switcher');
+
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.documentElement.classList.add('dark');
+}
+
+themeSwitcher.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark');
+    if (document.documentElement.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.removeItem('dark-mode');
+    }
+});
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-madgrower-main text-sm font-medium leading-5 text-madgrower-black dark:text-madgrower-white focus:outline-none focus:border-madgrower-main transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-madgrower-description dark:text-madgrower-description hover:text-madgrower-black dark:hover:text-madgrower-white hover:border-madgrower-main dark:hover:border-madgrower-main focus:outline-none focus:text-madgrower-black dark:focus:text-madgrower-white focus:border-madgrower-main dark:focus:border-madgrower-main transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
